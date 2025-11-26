3a4e0f47505ac8797ddc4b7fbaf7e5ff392a4f13
JorgeESala <jorge.e.sala@gmail.com>
2025-11-26 15:11:25 -0500

    added expense and expenseCategory api implementations

--- src/main/java/com/ocosur/ocosystem/controller/ExpenseController.java
+++ src/main/java/com/ocosur/ocosystem/controller/ExpenseController.java
@@ -5,19 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ocosur.ocosystem.dto.ExpenseDTO;
 import com.ocosur.ocosystem.model.Expense;
 import com.ocosur.ocosystem.service.ExpenseService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("/api/expenses")
 public class ExpenseController {
     @Autowired
@@ -26,11 +29,19 @@ public class ExpenseController {
     @GetMapping
     public ResponseEntity<List<Expense>> getExpenses() {
         return new ResponseEntity<List<Expense>>(expenseService.getExpenses(), HttpStatus.OK);
+    }   
+    @GetMapping("/{branchId}")
+    public ResponseEntity<List<Expense>> getExpensesByBranch(@PathVariable Integer branchId) {
+        return new ResponseEntity<List<Expense>>(expenseService.getLatestExpensesByBranch(), HttpStatus.OK);
     }
 
     @PostMapping()
-    public Expense saveExpense(@RequestBody Expense expense){
+    public Expense saveExpense(@RequestBody ExpenseDTO expense){
         return expenseService.saveExpense(expense);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Expense> updateExpense(@PathVariable Integer id, @RequestBody ExpenseDTO expenseDTO) {
+        return new ResponseEntity<Expense>(expenseService.updateExpense(id, expenseDTO), HttpStatus.OK);
     }
     
 }
--- src/main/java/com/ocosur/ocosystem/dto/ExpenseDTO.java
+++ src/main/java/com/ocosur/ocosystem/dto/ExpenseDTO.java
@@ -1,18 +1,12 @@
 package com.ocosur.ocosystem.dto;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.ocosur.ocosystem.model.Branch;
-import com.ocosur.ocosystem.model.ExpenseCategory;
-import lombok.Data;
 
-@Data
-public class ExpenseDTO {
-    Integer id;
-    String reason;
-    BigDecimal amount;
-    Branch branch;
-    LocalDate date;
-    ExpenseCategory category;
-}
+public record ExpenseDTO (
+    String reason,
+    BigDecimal amount,
+    Integer branchId,
+    LocalDate date,
+    Integer categoryId
+){}
