d05b37839e096cc3247b18c6e34690328665f805
JorgeESala <jorge.e.sala@gmail.com>
2025-12-04 12:24:32 -0500

    updated controller

--- src/main/java/com/ocosur/ocosystem/controller/ExpenseController.java
+++ src/main/java/com/ocosur/ocosystem/controller/ExpenseController.java
@@ -1,10 +1,8 @@
 package com.ocosur.ocosystem.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations.Search;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
